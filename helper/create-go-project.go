f2ecf85f9eaf67ff6c3c637198832c4841c76e8c
Rahul425 <rahulreddy425.rr@gmail.com>
2022-09-19 13:09:44 +0530

    add

--- helper/create-go-project.go
+++ helper/create-go-project.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"bytes"
 	"log"
 	"os/exec"
 )
@@ -19,6 +20,7 @@ func checkDependencies() error {
 }
 
 func CreateGolangProject(args []string, index int) {
+	var stdErr bytes.Buffer
 	// create a new golang project
 	// name := args[index+1]
 	// check if dependencies are installed
@@ -26,20 +28,26 @@ func CreateGolangProject(args []string, index int) {
 	if err := checkDependencies(); err != nil {
 		// print error message
 		log.Println("Dependencies are not installed i.e. go and git")
+		log.Println(err)
+		return
 	}
 	log.Println("Clone the repo")
 	// clone a folder from github
 	cmd := exec.Command("git", "clone", GITHUB_GO_CONFIGS["repo"])
+	cmd.Stderr = &stdErr
 	err := cmd.Run()
 	if err != nil {
-		log.Println(err)
+		log.Println(stdErr.String())
+		return
 	}
 	log.Println("Copying the boilerplate codes")
 	// copy the files from the cloned folder to the current directory
 	cmd = exec.Command("cp", GITHUB_GO_CONFIGS["create_template"], ".")
+	cmd.Stderr = &stdErr
 	err = cmd.Run()
 	if err != nil {
-		log.Println(err)
+		log.Println(stdErr.String())
+		return
 	}
 }
 
